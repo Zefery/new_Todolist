34983a15edb8a1ad3cb4126c10e819660fffa019
Zefery <rzefery@gmail.com>
2023-08-18 21:39:28 +0700

    view

--- resources/views/todolist.blade.php
+++ resources/views/todolist.blade.php
@@ -6,8 +6,8 @@
     <title>Todo list</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="index.css">
 </head>
 <body>
 
@@ -20,27 +20,30 @@
                     @csrf
                     <div class="input-group mb-3">
                         <input name="todo" type="text" class="form-control" placeholder="What do you want to do?">
-                        <button class="btn btn-primary" type="submit">simpan</button>
+                        <button class="btn btn-primary" type="submit"><i class="bi bi-save"></i></button>
                     </div>
                 </form>
                 
-                <ul class="list-group list-group-flush overflow-auto" style="max-height: 350px;">
+                <ul class="list-group list-group-horizontal-xxl list-group-flush overflow-auto" style="max-height: 350px;">
 
                     @foreach ($todos as $todo)
                     
-                        <li class="list-group-item d-flex justify-content-between bg-body-secondary {{( $todo ->status)? 'list-group-item-success' : '' }}">
-                            <a class="btn btn-light" href="/todo/delete/{{ $todo->id }}">
-                                <i class="bi bi-x-square"></i>
+                        <li class="list-group-item d-flex mb-3 rounded {{( $todo ->status)? 'list-group-item-success' : '' }}">
+                            
+                            <div class="me-auto p-2">
+                                @if ($todo->status) {{-- ($todo->status == true) --}}
+                                    <del>{{ $todo->todo }}</del> {{-- Mau test aja --}}                     
+                                @else
+                                    {{ $todo->todo }} 
+                                @endif
+                            </div>
+                            
+                            <a class="btn btn-light p-2 bg-{{( $todo->status)? 'success-subtle' : 'success' }}" style="margin-right: 10px" href="/todo/update/{{ $todo->id }}">
+                                <i class="bi-{{( $todo->status)? 'bi bi-recycle' : 'bi bi-check-square' }}"></i>
                             </a>
 
-                            @if ($todo->status) {{-- ($todo->status == true) --}}
-                                <del>{{ $todo->todo }}</del> {{-- Mau test aja --}}                     
-                            @else
-                                {{ $todo->todo }} 
-                            @endif
-
-                            <a class="btn btn-light" href="/todo/update/{{ $todo->id }}">
-                                <i class="bi-{{( $todo->status)? 'bi bi-recycle' : 'bi bi-check-square' }}"></i>
+                            <a class="btn btn-light p-2 bg-{{( $todo->status)? 'danger-subtle' : 'danger' }}" href="/todo/delete/{{ $todo->id }}">
+                                <i class="bi bi-x-square"></i>
                             </a>
                         </li>
 
